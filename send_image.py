1df8c09233b04f00a55738f61c84b2fdfbf22a0a
JocelynVelarde <a01285780@itesm.mx>
2024-08-10 20:14:18 -0600

    created script to send images using pywhatkit

--- send_image.py
+++ send_image.py
@@ -2,6 +2,7 @@ import pyautogui
 import time
 import re
 import os
+import pywhatkit
 
 def read_messages_from_file(file_path):
     with open(file_path, 'r', encoding='utf-8') as file:
@@ -25,27 +26,27 @@ def extract_messages(lines, sender_filter):
                     messages.append((sender, message, False))
     return messages
 
-def send_messages(messages, images_folder):
+def send_messages(messages, images_folder, phone_number):
     for sender, message, is_image in messages:
         if is_image:
             image_path = os.path.join(images_folder, message)
             if os.path.exists(image_path):
-                pyautogui.typewrite(f'{sender} sent an image: {message}')
-                pyautogui.press('enter')
-                time.sleep(1)
-                pyautogui.typewrite(image_path)
-                pyautogui.press('enter')
-                time.sleep(1)
+                pywhatkit.sendwhats_image(phone_number, image_path, f'{sender} sent an image: {message}')
+                time.sleep(5)  
             else:
                 print(f"Image {message} not found in {images_folder}")
         else:
-            pyautogui.typewrite(f'{sender}: {message}')
+            time.sleep(1)
+            
+            pyautogui.write(f'{sender}: {message}')
+            time.sleep(1)
+
             pyautogui.press('enter')
             time.sleep(1)
 
-
 file_path = 'chat.txt'
 images_folder = 'images/'
+phone_number = '+5' 
 lines = read_messages_from_file(file_path)
 messages = extract_messages(lines, 'Renato')
-send_messages(messages, images_folder)
+send_messages(messages, images_folder, phone_number)
