06e70d089e8b2babc9d73bb573ef127c3b7d966a
jpgtzg <58833781+jpgtzg@users.noreply.github.com>
2024-08-10 19:33:45 -0600

    Moved over to UDP P2P

--- server.py
+++ server.py
@@ -1,16 +1,29 @@
 import socket
 
-sock = socket.socket()
-sock.bind( ('localhost', 8001))
+known_port = 50002
 
-sock.listen(10)
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock.bind(('localhost', 55555))
 
 while True:
-    conn, addr = sock.accept()
-    print('connected: ', addr)
+    clients = []
 
-    conn.send(input('Enter message: ').encode())
-    
-    response = conn.recv(1024)
-    print(response.decode())
-    conn.close()
+    while True:
+        data, address = sock.recvfrom(128)
+
+        print('connection from: {}'.format(address))
+        clients.append(address)
+
+        sock.sendto(b'ready', address)
+
+        if len(clients) == 2:
+            print('got 2 clients, sending details to each')
+            break
+
+    c1 = clients.pop()
+    c1_addr, c1_port = c1
+    c2 = clients.pop()
+    c2_addr, c2_port = c2
+
+    sock.sendto('{} {} {}'.format(c1_addr, c1_port, known_port).encode(), c2)
+    sock.sendto('{} {} {}'.format(c2_addr, c2_port, known_port).encode(), c1)
